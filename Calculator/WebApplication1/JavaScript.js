36af72e5356cc400a93313c017282c2630e430a9
merisahakyan <sahakyan_m@bk.ru>
2017-02-27 19:12:45 +0400

    Calculator web

---
+++ Calculator/WebApplication1/JavaScript.js
@@ -0,0 +1,75 @@
+﻿function printValue() {
+    //document.getElementById("demo").value = "barev";
+    //document.getElementById("new_text").innerHTML = "<p> hello world </p>";
+    document.getElementById("text").value = eval(document.getElementById("text").value);
+}
+function getSymbols1() {
+    
+
+    document.getElementById("text").value = document.getElementById("text").value+"1";
+}
+function getSymbols2() {
+
+
+    document.getElementById("text").value = document.getElementById("text").value + "2";
+}
+function getSymbols3() {
+
+
+    document.getElementById("text").value = document.getElementById("text").value + "3";
+}
+function getSymbols4() {
+
+
+    document.getElementById("text").value = document.getElementById("text").value + "4";
+}
+function getSymbols5() {
+
+
+    document.getElementById("text").value = document.getElementById("text").value + "5";
+}
+function getSymbols6() {
+
+
+    document.getElementById("text").value = document.getElementById("text").value + "6";
+}
+function getSymbols7() {
+
+
+    document.getElementById("text").value = document.getElementById("text").value + "7";
+}
+function getSymbols8() {
+
+
+    document.getElementById("text").value = document.getElementById("text").value + "8";
+}
+function getSymbols9() {
+
+
+    document.getElementById("text").value = document.getElementById("text").value + "9";
+}
+function getSymbols10() {
+
+
+    document.getElementById("text").value = document.getElementById("text").value + "0";
+}
+function getSymbols11() {
+
+
+    document.getElementById("text").value = document.getElementById("text").value + "+";
+}
+function getSymbols12() {
+
+
+    document.getElementById("text").value = document.getElementById("text").value + "-";
+}
+function getSymbols13() {
+
+
+    document.getElementById("text").value = document.getElementById("text").value + "/";
+}
+function getSymbols14() {
+
+
+    document.getElementById("text").value = document.getElementById("text").value + "*";
+}
