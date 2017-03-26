21b5c7ae09633c6772477facb4899e64fc3ad5d1
merisahakyan <sahakyan_m@bk.ru>
2017-03-26 13:35:34 +0400

    WebPage

---
+++ WebPG/WebPG/JavaScript.js
@@ -0,0 +1,58 @@
+﻿function MenuButtonOver() {
+    var count = 3;
+    for (var i = 1; i <= count; i++) {
+        var btn = document.createElement("button");
+        btn.style.position = "absolute";
+        btn.style.backgroundColor = "purple";
+        btn.style.borderColor = "purple";
+        btn.style.left = "20px";
+        btn.style.top = 200 + 40 * i + "px";
+        btn.style.width = "120px";
+        btn.style.height = "40px";
+        btn.value = "Pizza";
+        btn.id = "newbutton" + i;
+        
+       
+
+        document.body.appendChild(btn);
+    }
+}
+
+
+function ProductsButtonOver(count) {
+    var count = 4;
+    for (var i = 1; i <= count; i++) {
+        var btn = document.createElement("button");
+        btn.style.position = "absolute";
+        btn.style.backgroundColor = "purple";
+        btn.style.borderColor = "purple";
+        btn.style.left = "140px";
+        btn.style.top = 200 + 40 * i + "px";
+        btn.style.width = "120px";
+        btn.style.height = "40px";
+        btn.value = "Pizza";
+        btn.id = "newbutton" + i;
+
+        document.body.appendChild(btn);
+    }
+}
+
+function CursorOut() {
+    var i = 1;
+    var btn;
+    var e = window.event;
+    var x = e.clientX;
+    var y = e.clientY;
+
+    if (x < 20 || x > 140 || y < 200 || y > 240) {
+
+        while (document.getElementById("newbutton" + i) != null) {
+            btn = document.getElementById("newbutton" + i);
+            document.body.removeChild(btn);
+            i++;
+        }
+
+    }
+
+}
+
---
+++ WebPG/WebPG/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WebPG")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WebPG")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d1ecf8aa-51ac-48df-92e3-71027bb01e8b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
